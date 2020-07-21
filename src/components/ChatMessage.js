36b3c0a6df18f6024247fad81d4c363cf57fb7b5
Hiba Z <hiba4.z@gmail.com>
2020-07-20 21:07:25 -0400

    workshop M4-1 COMPLETED

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,32 +2,41 @@ import React from "react";
 import Avatar from "./Avatar";
 import "./ChatMessage.css";
 
+function SentMessage(props) {
+  return (
+    <div className="chat-message-wrapper sent">
+      <div className="chat-message">
+        <div>{props.user.username}</div>
+        <Avatar size={36} user={props.user} />
+        <div className="chat-message">{props.body}</div>
+      </div>
+    </div>
+  );
+}
+
+function ReceivedMessage(props) {
+  return (
+    <div className="chat-message-wrapper received">
+      <div className="chat-message">
+        <div>{props.user.username}</div>
+        <Avatar size={36} user={props.user} />
+        <div className="chat-message">{props.body}</div>
+      </div>
+    </div>
+  );
+}
+
 function ChatMessage(props) {
+  console.log(props); // {user, body}
   if (props.messageType === "sent") {
-    return (
-      <div className="chat-message-wrapper sent">
-        {/* Sent */}
-        <section>
-          <div>{props.user.username}</div>
-          {/* <div className="chat-message-wrapper.sent"></div> */}
-          <Avatar size={36} user={props.user} />
-          <div className="chat-message">{props.body}</div>
-        </section>
-      </div>
-    );
+    return <SentMessage user={props.user} body={props.body} />;
   } else {
-    return (
-      <div className=".chat-message-wrapper received">
-        {/* Received */}
-        <section>
-          <div>{props.user.username}</div>
-          {/* <div className="chat-message-wrapper.received"></div> */}
-          <Avatar size={36} user={props.user} />
-          <div className="chat-message">{props.body}</div>
-        </section>
-      </div>
-    );
+    return <ReceivedMessage user={props.user} body={props.body} />;
   }
 }
 
 export default ChatMessage;
+
+//{
+// if (props.messageType === "sent") {
+//   return
